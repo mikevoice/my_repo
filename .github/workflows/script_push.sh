0021903946bf4b8b894804de9f21f6b728d4426e
Mikhail Murashevich <mikevoice@mail.ru>
2021-09-28 00:34:26 +0300

    edit script_push.sh

--- .github/workflows/script_push.sh
+++ .github/workflows/script_push.sh
@@ -7,7 +7,7 @@ git remote add origin git@github.com:mikevoice/my_repo.git
 git push origin --all
 #
 #
-echo " Push to Bitbucket
+echo " Push to Bitbucket"
 Git remote add bitbuck git@bitbucket.org:mikevoice/my_repo.git
 git push bitbuck --all
 #
