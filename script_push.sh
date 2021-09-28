80e3833454eda6d89820fb2dd05aac7383ad1836
Mikhail Murashevich <mikevoice@mail.ru>
2021-09-28 20:35:10 +0300

    edit 03.GIT.Hosting

--- script_push.sh
+++ script_push.sh
@@ -6,7 +6,6 @@ echo "*******************"
 echo "*  Push to GitHub *"
 echo "*******************"
 #
-git remote add origin git@github.com:mikevoice/my_repo.git
 git push origin --all
 #
 #
@@ -14,12 +13,11 @@ echo "***********************"
 echo "*  Push to Bitbucket  *"
 echo "***********************"
 #
-Git remote add bitbuck git@bitbucket.org:mikevoice/my_repo.git
 git push bitbuck --all
 #
 #
 echo "********************"
 echo "*  Push to GitLab  *"
 echo "********************"
-git remote add gitlab git@gitlab.com:mikevoice/my_repo.git
+#
 git push gitlab --all
